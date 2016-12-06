bd4c4fa355dddf0551487b62870c39079049d3be
ikac <ikac@isd.dp.ua>
2016-12-06 16:02:26 +0200

    Enable cache for offline.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
