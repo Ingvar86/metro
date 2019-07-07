2340cea700007077ec76087cc30f65ef69b43f0d
Ingvar86 <gara314@gmail.com>
2019-07-07 13:44:28 +0300

    Add compression

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict'
 var express = require('express');
+var compression = require('compression')
 var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
