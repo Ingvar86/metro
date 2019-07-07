37232fc66ecfa584380780ad303851923d96f2d0
Ingvar86 <gara314@gmail.com>
2019-07-07 15:59:54 +0300

    Add service worker

--- public/index.js
+++ public/index.js
@@ -1,5 +1,18 @@
 (function(){
-window.onload = tick;
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+window.addEventListener('load', tick);
 
 var timeTable = {
     vokzalna:
