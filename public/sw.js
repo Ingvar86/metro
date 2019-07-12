3f1e9111f5bec9ff3cc57bf4ba41547c808d7e88
ihor-kachura-neyber <ihor.kachura@neyber.co.uk>
2019-07-12 11:22:29 +0300

    Cache optimization

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
-var CACHE_NAME = 'metro-cache-v5';
+var CACHE_NAME = 'metro-cache-v6';
 var urlsToCache = [
     '/',
+    '/images/metro-192.png',
     '/index.html',
-    '/index.js'
+    '/index.js',
+    '/manifest.json'
 ];
 
 self.addEventListener('install', function(event) {
@@ -24,7 +26,27 @@ self.addEventListener('fetch', function(event) {
           if (response) {
             return response;
           }
-          return fetch(event.request);
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
         }
       )
     );
