00bcdadac678f1ee440c1576d6c8d90c37331313
Ingvar86 <gara314@gmail.com>
2019-11-23 13:36:22 +0200

    Correct timetable

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'metro-cache-v4';
+var CACHE_NAME = 'metro-cache-v5';
 var urlsToCache = [
     '/',
     '/index.html',
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 'use strict'
 var express = require('express');
-var compression = require('compression')
 var app = express();
 
 var port = process.env.PORT || 8088;
 
-app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function () {
